24753bd1afb5f246bb917cd97f455d8c16b5cc51
rvaldez20 <rvaldez20@gmail.com>
2024-03-09 17:03:47 -0600

    [ADD] hacemos response en formatpo texto y json

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,18 @@ const server = http.createServer((req, res) => {
 
    console.log(req.url);
 
-   res.write('Hola Mundo');
-   res.end();
+   //! Serve Side Rendering (res->html)
+   // res.writeHead(200,{ 'Content-Type': 'text/html' });
+   // res.write(`<h1>URL ${req.url}</h1>`);
+   // res.end();
+
+   //! Serve Side Rendering (res->json)
+   // const data = {name:'Jonh Doe', age: 30, city: 'New York'};
+   // res.writeHead(200, { 'Content-Type': 'application/json' });
+   // // write() y end() en una misma linae
+   // res.end( JSON.stringify(data) );
+
+
 
 });
 
