706be3351c476eb3ce41a03980c3f649900b4a09
rvaldez20 <rvaldez20@gmail.com>
2024-03-09 17:43:12 -0600

    [ADD] servimos ademas de html tambien css y js

--- src/app.ts
+++ src/app.ts
@@ -13,19 +13,27 @@ const server = http.createServer((req, res) => {
 
   //! Serve Side Rendering (res->json)
   // const data = {name:'Jonh Doe', age: 30, city: 'New York'};
-  // res.writeHead(200, { 'Content-Type': 'application/json' });
+  // res.writeHead(200, { 'Content-Type': 'application  /json' });
   // // write() y end() en una misma linae
   // res.end( JSON.stringify(data) );
 
   if( req.url === '/') {
     const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write(htmlFile);
     res.end( htmlFile );
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end();
+
+    return;
   }
 
+  if( req.url?.endsWith('.js') ) {
+    res.writeHead(200, { 'Content-Type': 'application/javascript' });
+  } else if( req.url?.endsWith('.css') ) {
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+  }
+
+  const responseContent = fs.readFileSync(`./public${ req.url }`, 'utf-8');
+  res.end( responseContent );
 
 });
 
