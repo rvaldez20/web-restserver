c103a3d1e211ceb8dc3f822aed74a677c361fc99
rvaldez20 <rvaldez20@gmail.com>
2024-03-09 17:18:23 -0600

    [ADD] expandimos el web server para servir un archivo html

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,30 @@
 
 import http from 'http';
+import fs from 'fs'
 
 const server = http.createServer((req, res) => {
 
-   console.log(req.url);
+  console.log(req.url);
 
-   //! Serve Side Rendering (res->html)
-   // res.writeHead(200,{ 'Content-Type': 'text/html' });
-   // res.write(`<h1>URL ${req.url}</h1>`);
-   // res.end();
+  //! Serve Side Rendering (res->html)
+  // res.writeHead(200,{ 'Content-Type': 'text/html' });
+  // res.write(`<h1>URL ${req.url}</h1>`);
+  // res.end();
 
-   //! Serve Side Rendering (res->json)
-   // const data = {name:'Jonh Doe', age: 30, city: 'New York'};
-   // res.writeHead(200, { 'Content-Type': 'application/json' });
-   // // write() y end() en una misma linae
-   // res.end( JSON.stringify(data) );
+  //! Serve Side Rendering (res->json)
+  // const data = {name:'Jonh Doe', age: 30, city: 'New York'};
+  // res.writeHead(200, { 'Content-Type': 'application/json' });
+  // // write() y end() en una misma linae
+  // res.end( JSON.stringify(data) );
 
+  if( req.url === '/') {
+    const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end( htmlFile );
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end();
+  }
 
 
 });
