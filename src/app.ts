ae6d3f01b27c83f64711f48cfe45e10fae22bd26
rvaldez20 <rvaldez20@gmail.com>
2024-03-08 20:39:01 -0600

    [ADD] Creamos un rest server http basico

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
 
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+
+   console.log(req.url);
+
+   res.write('Hola Mundo');
+   res.end();
+
+});
+
+
+server.listen(8080, () => {
+   console.log('Server run in Port 8080');
+});
