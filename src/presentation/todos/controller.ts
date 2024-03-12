5dc5502b2e074b07a7ba23e631953fb62ad9c82c
rvaldez20 <rvaldez20@gmail.com>
2024-03-12 00:27:40 -0600

    [ADD] se crean los archivos independientes para las routes y los controllers de los todos

---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express"
+
+
+export class TodosController {
+
+  //* DI
+  constructor() {}
+
+
+  public getTodos = (req: Request, res: Response) => {
+
+    res.json([
+      { id: 1, text: 'Buy milk', createdAt: new Date()},
+      { id: 2, text: 'Buy bread', createdAt: null},
+      { id: 3, text: 'Buy butter', createdAt: new Date()},
+    ])
+
+  }
+
+
+}
