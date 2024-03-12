0ff6d4f3cff2c406e6865caa03e09abee3a22b42
rvaldez20 <rvaldez20@gmail.com>
2024-03-11 19:03:52 -0600

    [ADD] se configura el server para servir aunque se recargue la pagina

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path';
 
 
 export class Server {
@@ -14,6 +15,15 @@ export class Server {
 
     //* public folder
     this.app.use(express.static('public'));
+
+    this.app.get('*', (req, res) => {
+      const indexPath = path.join( __dirname + '../../../public/index.html' );
+      console.log(indexPath);
+      res.sendFile(indexPath);
+
+      // console.log(req.url);
+      // res.send('Hola Mundo')
+    })
 
 
     this.app.listen(4000, () => {
