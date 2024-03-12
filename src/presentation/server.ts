6500d3ac973cc72af835c7ebafbe240272803ba0
rvaldez20 <rvaldez20@gmail.com>
2024-03-11 19:29:30 -0600

    [ADD] se configura todo lo de las variables de entorno como inyeccion de dependncias: port y publiPath

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,10 +1,24 @@
 import express from 'express'
 import path from 'path';
 
+interface Options {
+  port: number
+  public_path?: string
+}
+
 
 export class Server {
 
   private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
+
+
+  constructor(options: Options) {
+    const { port, public_path = 'public' } = options;
+    this.port = port;
+    this.publicPath = public_path;
+  }
 
 
   async start() {
@@ -14,10 +28,10 @@ export class Server {
 
 
     //* public folder
-    this.app.use(express.static('public'));
+    this.app.use(express.static(this.publicPath));
 
     this.app.get('*', (req, res) => {
-      const indexPath = path.join( __dirname + '../../../public/index.html' );
+      const indexPath = path.join( __dirname + `../../../${this.publicPath}/index.html` );
       console.log(indexPath);
       res.sendFile(indexPath);
 
@@ -26,8 +40,8 @@ export class Server {
     })
 
 
-    this.app.listen(4000, () => {
-      console.log(`Server running on port ${4000}`);
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
     });
 
   }
