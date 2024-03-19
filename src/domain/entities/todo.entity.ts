dee7b7d935fc7ef56dd9875a714fba515189f66c
rvaldez20 <rvaldez20@gmail.com>
2024-03-18 19:49:51 -0600

    [ADD] se hace la implementacion el method getAll()

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -13,4 +13,22 @@ export class TodoEntity {
       return !!this.completedAt;
    }
 
+   public static fromObj(object: {[key: string]: any}): TodoEntity {
+    const {id, text, completedAt} = object;
+
+    //* VALIDACIONES
+    if( !id ) throw 'Id is required';
+    if( !text ) throw 'text is required';
+
+    let newCompetedAt;
+    if( completedAt ) {
+      newCompetedAt = new Date( completedAt );
+      if(isNaN(newCompetedAt.getTime()))
+        throw 'CompetedAt is not a value date';
+    }
+
+    //* Creamos la Instancia
+    return new TodoEntity( id, text, completedAt );
+   }
+
 }
