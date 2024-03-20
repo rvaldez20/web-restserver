39717a89bd02e1a2af94eba1aaf175ea59cac81e
rvaldez20 <rvaldez20@gmail.com>
2024-03-19 22:33:39 -0600

    [ADD] implementaos los use case de los methods del CRUD

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 import { prisma } from '../../data/postgres/index';
 import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
-import { TodoRepository } from "../../domain";
+import { CreateTodo, DeleteTodo, GetTodo, GetTodos, TodoRepository, UpdateTodo } from "../../domain";
 
 
 
@@ -13,63 +13,59 @@ export class TodosController {
   ) {}
 
   //! Get all todos
-  public getTodos = async(req: Request, res: Response) => {
-    const todos = await this.todoRepository.getAll();
-    //* cada todo son instancia de TodoEntity
-    // console.log({todos});
-    return res.json(todos);
+  public getTodos = (req: Request, res: Response) => {
+    // implemet use case
+    new GetTodos( this.todoRepository)
+      .execute()
+      .then( todos => res.json(todos) )
+      .catch( error => res.status(400).json({error}) )
   }
 
   //! Get a todo by id
-  public getTodoById = async(req:Request, res: Response) => {
+  public getTodoById = (req:Request, res: Response) => {
     const id = +req.params.id;
 
-    try {
-      const todo = await this.todoRepository.findById(id);
-      return res.json(todo);
-    } catch (error) {
-      res.status(400).json({error});
-    }
+    // implemet use case
+    new GetTodo(this.todoRepository)
+      .execute( id )
+      .then( todo => res.json(todo))
+      .catch( error => res.status(400).json({error}))
   }
 
-  public createTodo = async(req: Request, res: Response) => {
+  public createTodo = (req: Request, res: Response) => {
     const [error, createTodoDto] = CreateTodoDto.create(req.body);
-    if(error) return res.status(400).json({ error });
+    if(error) return res.status(400).json({error});
 
-    try {
-      const todo = await this.todoRepository.create(createTodoDto!);
-      return res.json(todo);
-    } catch (error) {
-      res.status(400).json({error});
-    }
+    // implemet use case
+    new CreateTodo(this.todoRepository)
+      .execute( createTodoDto! )
+      .then( todo => res.json(todo))
+      .catch( error => res.status(400).json({error}))
   }
 
 
-  public updateTodo = async(req:Request, res: Response) => {
+  public updateTodo = (req:Request, res: Response) => {
     const id = +req.params.id;
     const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id })
     if (error) return res.status(400).json({error});
 
-    try {
-      const todoUpdate = await this.todoRepository.updateById(updateTodoDto!)
-      return res.json(todoUpdate);
-    } catch (error) {
-      res.status(400).json({error});
-    }
+    // implemet use case
+    new UpdateTodo(this.todoRepository)
+      .execute(updateTodoDto!)
+      .then( todo => res.json(todo))
+      .catch( error => res.status(400).json({error}))
   }
 
 
 
-  public deleteTodo = async(req:Request, res:Response) => {
+  public deleteTodo = (req:Request, res:Response) => {
     const id = +req.params.id;
-    if(isNaN(id)) return res.status(400).json({ err: 'ID argument need to be a number'});
 
-    try {
-      const todoDeleted = await this.todoRepository.deleteById(id);
-      return res.json(todoDeleted);
-    } catch (error) {
-      res.status(400).json({error});
-    }
+    // implemet use case
+    new DeleteTodo(this.todoRepository)
+      .execute( id )
+      .then( todo => res.json(todo))
+      .catch( error => res.status(400).json({error}))
   }
 
 
